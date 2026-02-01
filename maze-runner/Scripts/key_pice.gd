a26a025cae42ada0c17ce15cda31abb7a0a503aa
LOL_1243 <lol1243.001@gmail.com>
2026-02-01 16:41:48 +0100

    key pece pick up sound

--- maze-runner/Scripts/key_pice.gd
+++ maze-runner/Scripts/key_pice.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var pice_number : String
 
+var picked_up = true
+
 func _ready( ) -> void:
 	
 	$AnimatedSprite2D.play( pice_number )
@@ -10,8 +12,12 @@ func _ready( ) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	
-	if body is CharacterBody2D:
+	if body is CharacterBody2D and picked_up:
+		
+		$AudioStreamPlayer2D.play( )
 		
 		SignalBus.key_pice_amount_picket_up += 1
 		
-		queue_free()
+		visible = false
+		
+		picked_up = false
